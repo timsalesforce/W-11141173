1c07edbca13985ca951ceeb1f4e566330e2151ef
Timothy <tdionne@salesforce.com>
2022-05-27 11:14:29 -0700

    Init

---
+++ select Id, CreatedDate, Insights_Remote_.sql
@@ -0,0 +1,4 @@
+select Id, CreatedDate, Insights_Remote_Collection__c  FROM Cluster__c Where SystemModstamp >= 2022-05-23T16:49:37Z and SystemModstamp <= 2022-05-25T16:49:39Z ORDER BY CreatedDate DESC LIMIT 12
+select Id, CreatedDate, Insights_Remote_Collection__c  FROM Cluster__c Where CreatedDate <= 2022-05-25T12:41:49Z AND CreatedDate >= 2022-05-25T12:41:48Z
+
+https://workbench.internal.salesforce.com/retrieve.php?id=a1S7V00000GPfxqUAD
